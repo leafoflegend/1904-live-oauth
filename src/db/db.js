b5c4901e99f143fb3edd3a19baa13f255a44932e
Eliot Szwajkowski <eliotpszw@gmail.com>
2019-07-23 23:17:46 -0400

    Touch ups.

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URI || 'postgres://localhost:5432/live-oauth-1904', {
+const db = new Sequelize(process.env.DATABASE_URI || 'postgres://localhost:5432/1904-live-oauth', {
   logging: false,
 });
 
